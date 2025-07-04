96fe67d83235c600485dc605a8acc0a273e1450c
Alexander <linsalo19b@gmail.com>
2025-07-04 16:13:56 -0500

    Implementación inicial de login con Google

--- config/services.php
+++ config/services.php
@@ -50,5 +50,10 @@ return [
         'province' => env('COMPANY_PROVINCE'),
         'department' => env('COMPANY_DEPARTMENT'),
     ],
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
 
 ];
