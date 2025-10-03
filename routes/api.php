7ff7c615e1e5c6dafc14f8f04c7f065a68df0147
EmerRo <yarlequerodrigo9@gmail.com>
2025-10-03 10:59:36 -0500

    cambios

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\BannersController;
 use App\Http\Controllers\BannersPromocionalesController;
+use App\Http\Controllers\BannerFlashSalesController;
+use App\Http\Controllers\BannerOfertaController;
 use App\Http\Controllers\CuponesController;
 use App\Http\Controllers\EmpresaInfoController;
 use App\Http\Controllers\MarcaProductoController;
@@ -73,6 +75,8 @@ Route::get('/productos/buscar', [ProductosController::class, 'buscarProductos'])
 Route::get('/categorias-sidebar', [ProductosController::class, 'categoriasParaSidebar']);
 Route::get('/banners/publicos', [BannersController::class, 'bannersPublicos']);
 Route::get('/banners-promocionales/publicos', [BannersPromocionalesController::class, 'bannersPromocionalesPublicos']);
+Route::get('/banners-flash-sales/activos', [BannerFlashSalesController::class, 'activos']);
+Route::get('/banners-ofertas/activo', [BannerOfertaController::class, 'getBannerActivo']);
 Route::get('/marcas/publicas', [MarcaProductoController::class, 'marcasPublicas']);
 Route::get('/marcas/por-categoria', [MarcaProductoController::class, 'marcasPorCategoria']);
 
@@ -312,6 +316,47 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::middleware('permission:banners_promocionales.delete')->group(function () {
         Route::delete('/banners-promocionales/{id}', [BannersPromocionalesController::class, 'destroy']);
+    });
+
+    // Protección de rutas del módulo banners flash sales con sus respectivos permisos
+    Route::middleware('permission:banners_flash_sales.ver')->group(function () {
+        Route::get('/banners-flash-sales', [BannerFlashSalesController::class, 'index']);
+        Route::get('/banners-flash-sales/{id}', [BannerFlashSalesController::class, 'show']);
+    });
+
+    Route::middleware('permission:banners_flash_sales.create')->group(function () {
+        Route::post('/banners-flash-sales', [BannerFlashSalesController::class, 'store']);
+    });
+
+    Route::middleware('permission:banners_flash_sales.edit')->group(function () {
+        Route::post('/banners-flash-sales/{id}', [BannerFlashSalesController::class, 'update']);
+        Route::patch('/banners-flash-sales/{id}/toggle-activo', [BannerFlashSalesController::class, 'toggleActivo']);
+    });
+
+    Route::middleware('permission:banners_flash_sales.delete')->group(function () {
+        Route::delete('/banners-flash-sales/{id}', [BannerFlashSalesController::class, 'destroy']);
+    });
+
+    // Protección de rutas del módulo banners ofertas con sus respectivos permisos
+    Route::middleware('permission:banners_ofertas.ver')->group(function () {
+        Route::get('/banners-ofertas', [BannerOfertaController::class, 'index']);
+        Route::get('/banners-ofertas/{id}', [BannerOfertaController::class, 'show']);
+        Route::get('/banners-ofertas/{bannerId}/productos', [BannerOfertaController::class, 'show']);
+    });
+
+    Route::middleware('permission:banners_ofertas.create')->group(function () {
+        Route::post('/banners-ofertas', [BannerOfertaController::class, 'store']);
+        Route::post('/banners-ofertas/{id}/productos', [BannerOfertaController::class, 'agregarProductos']);
+    });
+
+    Route::middleware('permission:banners_ofertas.edit')->group(function () {
+        Route::post('/banners-ofertas/{id}', [BannerOfertaController::class, 'update']);
+        Route::patch('/banners-ofertas/{bannerId}/productos/{productoId}', [BannerOfertaController::class, 'actualizarDescuentoProducto']);
+    });
+
+    Route::middleware('permission:banners_ofertas.delete')->group(function () {
+        Route::delete('/banners-ofertas/{id}', [BannerOfertaController::class, 'destroy']);
+        Route::delete('/banners-ofertas/{bannerId}/productos/{productoId}', [BannerOfertaController::class, 'quitarProducto']);
     });
 
     // Rutas de clientes protegidas con permisos
